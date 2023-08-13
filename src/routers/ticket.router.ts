c3f25000b8971c043759d268ebb2aa7e3dd649f8
AIDXYLELELE <warboyka48@gmail.com>
2023-08-13 11:02:40 +0200

    feat: Implemented Ticket Router

--- src/routers/ticket.router.ts
+++ src/routers/ticket.router.ts
@@ -1,1 +1,37 @@
 import { Router } from "express";
+import { authenticate, errorMiddleware } from "ticketwing-storage-util";
+import { responseMiddleware } from "../middlewares/response.middleware";
+import { TicketController } from "../controllers/ticket.controller";
+
+const ticketController = new TicketController();
+export const ticketRouter = Router();
+
+ticketRouter.use(authenticate);
+
+ticketRouter.post(
+  "search",
+  responseMiddleware(ticketController.searchTickets.bind(ticketController)),
+  errorMiddleware
+);
+
+ticketRouter.get(
+  "details/:id",
+  responseMiddleware(ticketController.getTicketDetails.bind(ticketController)),
+  errorMiddleware
+);
+
+ticketRouter.get(
+  "favorites",
+  responseMiddleware(
+    ticketController.getFavoriteTickets.bind(ticketController)
+  ),
+  errorMiddleware
+);
+
+ticketRouter.post(
+  "favorites",
+  responseMiddleware(
+    ticketController.addTicketToFavorite.bind(ticketController)
+  ),
+  errorMiddleware
+);
